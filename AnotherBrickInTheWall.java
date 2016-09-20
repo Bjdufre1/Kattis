93a0f4e5b110c6400d6a89829127d6b75ed2e755
Brady J. Dufresne <bjdufre1@csis.svsu.edu>
2016-09-20 14:46:04 -0400

    I give up

---
+++ AnotherBrickInTheWall.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+/**
+ * Created by bjdufre1 on 9/20/2016.
+ */
+public class AnotherBrickInTheWall {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int heightOfWall = sc.nextInt();
+        int lengthOfWall = sc.nextInt();
+        int numberOfBricks = sc.nextInt();
+        int[] arrayOfSizes = new int[numberOfBricks];
+        for(int i = 0; i < numberOfBricks; i++){
+            arrayOfSizes[i] = sc.nextInt();
+        }
+        int loopLength = 0;
+        int a = 0;
+        while(loopLength < lengthOfWall){
+
+        }
+    }
+}
